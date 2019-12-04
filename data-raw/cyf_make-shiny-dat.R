d5e367b96e33100e0a9ae0a414a1eeba4d512f62
vanichols <vnichols@iastate.edu>
2019-12-03 20:59:52 -0600

    Playing with Shiny (GN)

--- data-raw/cyf_make-shiny-dat.R
+++ data-raw/cyf_make-shiny-dat.R
@@ -8,7 +8,7 @@
 #########################
 
 library(tidyverse)
-
+library(ggpubr)
 
 exraw <- read_csv("data-raw/_tidy/cyd_salprofs.csv") %>% 
   #Just keep 'colleges'
@@ -35,6 +35,11 @@ coldept <- exraw %>%
   select(fiscal_year, college, dept) %>% 
   distinct()
 
+
+coldept2 <- exraw %>% 
+  select(college, dept) %>% 
+  distinct()
+
 # assign ------------------------------------------------------------------
 
 nomf <- 
@@ -42,6 +47,8 @@ nomf <-
   mutate(id = ifelse(is.na(`F`), "No Females",
                      ifelse(is.na(M), "No Males", NA))) %>% 
   select(fiscal_year, dept, prof_simp, id) 
+
+
 
 dat <- combs %>% 
   left_join(nomf) %>% 
@@ -54,10 +61,59 @@ dat %>%
   geom_tile(aes(fill = id)) + 
   coord_flip()
 
+# write it ----------------------------------------------------------------
+
+dat %>% write_csv("data-raw/_tidy/cyd_missing-genders.csv")
+
+
+# trying something new ----------------------------------------------------
+
+dat <- 
+  # Get all combinations of prof/depts
+  combs %>% 
+  left_join(exraw) %>% 
+  select(-college) %>% 
+  # assign college
+  left_join(coldept2) %>% 
+  # assign gender verdict
+  mutate(id = ifelse((is.na(`F`) & is.na(M)), "No One",
+                     ifelse(is.na(`F`), "No Females",
+                            ifelse(is.na(M), "No Males", "Both Genders Present")))) %>% 
+  mutate(ndisplay = ifelse(id == "Both Genders Present", M + `F`,
+                           ifelse( id == "No One", NA, 
+                                   ifelse(id == "No Females", M, `F`))))
+
 
 # write it ----------------------------------------------------------------
 
 dat %>% write_csv("data-raw/_tidy/cyd_missing-genders.csv")
+
+dat %>% 
+  filter(grepl("ag", college)) %>% 
+  filter(fiscal_year == 2018) %>% 
+  ggplot(aes(x = dept, y = prof_simp)) +
+  geom_tile(aes(fill = id)) +
+  geom_point(aes(size = ndisplay)) +
+  scale_fill_manual(values = c(`No Males` = "darkorchid1",
+                               `No Females` = "goldenrod", 
+                               `Both Genders Present` = "gray90",
+                               `No One` = "gray10")) +
+  coord_flip() +
+  labs(
+    x = NULL,
+    y = NULL,
+    fill = NULL) +
+  #theme_bw() +
+  theme_pubclean() +
+  facet_grid(~ college) + 
+  
+  theme(strip.text = element_text(size = rel(1.3), color = "white"),
+        strip.background = element_rect(fill = "black"),
+        axis.title = element_text(size = rel(1.3)),
+        axis.text.x = element_text(size = rel(1.3)),
+        legend.position = "top",
+        legend.background = element_rect(linetype = "solid", color = "black"))
+
 
 library(ggpubr)
 # practice shiny plot -----------------------------------------------------
