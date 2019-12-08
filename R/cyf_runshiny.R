f1ba486f475758b8d145b603a5b354d98de6889f
Lydia English <lenglish@iastate.edu>
2019-12-08 15:04:18 -0600

    More tweaking on presentation (I could do this forever...)

---
+++ R/cyf_runshiny.R
@@ -0,0 +1,17 @@
+#' @name cy_RunShiny
+#' @title Run Shiny App
+#' @description Launch Shiny App that displays pay information for subsets of Iowa State University Employees
+#' @return a Shiny App
+#' @examples
+#' \dontrun{
+#' cy_RunShiny()
+#' }
+#' @export
+
+cy_RunShiny <- function() {
+  appDir <- system.file("shiny-examples", "myapp", package = "CyChecks2")
+  if (appDir == ""){
+    stop("Couldn't locate example directory. Try re-installing `CyChecks`", call. = FALSE)
+  }
+  shiny::runApp(appDir, display.mode = "normal")
+}
