4005b80b4e10de3c0d34ad138e65cc0342a6f7d2
Lydia English <lenglish@iastate.edu>
2020-02-27 09:18:26 -0600

    Updated simplify_profs function to work with`salaries` data

--- R/simplify_profs.R
+++ R/simplify_profs.R
@@ -1,29 +1,35 @@
-#' Simplify professor titles
+#' Simplify professor titles for easy interpretation
 #'
-#' @param data A dataframe that can be simplified. 
+#' @param data A dataframe with professor titles. 
 #'
 #' @return A dataframe without adjunct, affiliation, emeritus, visiting, collaborating or department chairs, 
-#' as their salaries tend to be weird. 
+#' as their salaries tend to be weird, and a new column named `prof_simp` that has simplified names, see below. 
+#' 
+#' @details This function filters all titles to only include `PROF`, `ASSOC PROF`, and `ASST PROF`, and creates
+#' a new category called `AWARDED PROF` which includes distinguished, morrill or university professors. 
+#' 
 #' @export
 #'
 #' @examples
-#' 
+#' data(salaries)
+#' simplify_profs(data = salaries)
 #' 
 
-simplify_profs <- function(data = cyd_saldept){
+simplify_profs <- function(data = data){
   
-  awardprof <- c("distg prof", "univ prof", "morrill prof")
+  assertthat::see_if("title" %in% names(data), msg = "There needs to be a `title` column to simplify profs")
+  
+  awardprof <- stringr::str_to_upper(c("distg prof", "univ prof", "morrill prof"))
   
   df <- data %>%
   
   # eliminate department chairs, they are weird
   # eliminate adjuncts, things with 'adj' or 'affil' or 'emer' or 'vstg', they are paid strangely
   
-  filter(!grepl("chair|adj|affil|emer|vstg|chr|clin|collab|res", title)) %>%
-  filter(grepl("prof", title)) %>%
+  dplyr::filter(!grepl(stringr::str_to_upper("chair|adj|affil|emer|vstg|chr|clin|collab|res"), title)) %>%
+  dplyr::filter(grepl("PROF", title)) %>%
   
-  mutate(prof_simp = ifelse(position %in% awardprof, "awarded prof", title)) %>% 
-  filter(!is.na(dept))
+  dplyr::mutate(prof_simp = ifelse(title %in% awardprof, "AWARDED PROF", title))
   
   return(df)
   
