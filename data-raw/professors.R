cc8c7032368b44570be3a21ff4d675ca9c6d6c99
Jarad Niemi <niemi@iastate.edu>
2020-02-27 16:37:41 -0800

    Edit professors.R - work in progress

--- data-raw/professors.R
+++ data-raw/professors.R
@@ -2,28 +2,44 @@
 # last_name first_name middle_initial
 # 
 key_from_name <- function(x) {
-  tolower(sub("^(\\S*\\s+\\S+).*", "\\1", x)) # keep up to 2nd space
+  tolower(sub("^(\\S*\\s+\\S+\\s+\\S+).*", "\\1", x)) # keep up to 3rd space
 }
 
 load("../data/affiliation.rda")
 load("../data/departments.rda")
 load("../data/salaries.rda")
 
+
+
+
 professors <- salaries %>%
-  mutate(position = tolower(position)) %>%
+  mutate(title = tolower(title)) %>%
   
-  # filter(grepl("prof", position)) # could do this
-  filter(position %in% c("asst prof","assoc prof","prof")) %>%
-  rename(rank = position) %>%
+  filter(grepl("prof", title)) %>% 
 
   mutate(key = key_from_name(name)) %>%
   # select(-name) %>% 
-  left_join(affiliation %>% 
-              mutate(key = key_from_name(name)),
-            by = "key") 
+  left_join(affiliation  %>%
+              mutate(key = key_from_name(name)) %>% 
+              select(-name),
+            by = c("key","year")) 
 
-professors %>% 
-  group_by(year,key) %>%
+
+
+
+
+
+# Check to make sure professors are unique
+tmp <- professors %>%
+  group_by(year,key,name) %>%
   summarize(n = n()) %>%
-  ungroup() %>%
+  ungroup()
+
+tmp  %>%
+  summarize(maxn = max(n)) # should be 1
+
+tmp %>%
+  arrange(-n)
+
   
+# usethis::use_data(professors, overwrite = TRUE)
