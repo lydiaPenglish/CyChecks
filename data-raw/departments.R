ad2cd7578a7982516c9b6070692e48722e84eb2a
vanichols <vnichols@iastate.edu>
2020-04-26 18:33:29 -0500

    add note to deptarments data about 7090, doesn't change anything

--- data-raw/departments.R
+++ data-raw/departments.R
@@ -30,6 +30,9 @@ departments <- readr::read_csv("departments/dept_title.csv",
 # row                col   expected actual                         file
 # 261 PARENT DEPT NUMBER an integer        'departments/dept_title.csv'
 
+
+# I propose we copy-paste 7040 and assign it a value of 7090
+
 usethis::use_data(departments, overwrite = TRUE)
 
 
@@ -44,3 +47,17 @@ department_rename <- readr::read_csv("departments/department_rename.csv",
 
 usethis::use_data(department_rename, overwrite = TRUE)
 
+
+################################################################################
+# why are some depts missing
+#--John Cunnally seems like a 'dept transition' problem, 7090 in 2013-2015 is blank
+# 7050 before that, 7040 afterwards
+# 
+# Beginning July 1, 2012, the department of Art and Design was divided into four departments – Integrated Studio Arts, Industrial
+# Design, Interior Design and Graphic Design. There was not sufficient peer data available to separate these departments so they are
+# combined in this report.
+
+departments %>% 
+  filter(DEPT1 %in% c(7090, 7040, 7050))
+
+# NOTE: did nothing to this data, assigned 7090 to "ART/VISUAL CULT" in affiliation data         
